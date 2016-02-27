f208ec87748c1c23c48cce830bd1f77208df6979
Adam Novotny <1@adamnovotny.com>
2016-02-26 19:03:15 -0500

    Update email_support import

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
+# run setup of Flask app
 
-# email
-MAIL_SERVER = 'smtp-mail.outlook.com'
-MAIL_PORT = 25
-MAIL_USERNAME = 'citysketch@outlook.com'
-MAIL_PASSWORD = '495team3'
-MAIL_USE_TLS = True
-MAIL_USE_SSL = False
+def setup_env(app):
+    app.config['MAIL_SERVER'] = 'smtp-mail.outlook.com'
+    app.config['MAIL_PORT'] = 25
+    app.config['MAIL_USERNAME'] = 'citysketch@outlook.com'
+    app.config['MAIL_PASSWORD']= '495team3'
+    app.config['MAIL_USE_TLS'] = True
+    app.config['MAIL_USE_SSL'] = False
