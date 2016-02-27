469cf277132c5a8e9ef0bac91e934a1067649351
Adam Novotny <1@adamnovotny.com>
2016-02-26 20:05:58 -0500

    Add message flashing

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 # run setup of Flask app
 
 def setup_env(app):
+    # general
+    app.secret_key = 'A0Zasdfsafdsjlfjadfs9892834234[,;234'
+
+    # email
     app.config['MAIL_SERVER'] = 'smtp-mail.outlook.com'
     app.config['MAIL_PORT'] = 25
     app.config['MAIL_USERNAME'] = 'citysketch@outlook.com'
